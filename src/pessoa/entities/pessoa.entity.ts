e8ba4af2c885bf3ab7249212982028ebc1383a4c
Carlos Eduardo <cadumouralemes@gmail.com>
2025-06-16 14:35:24 -0300

    feat: Pessoa initial db entity

--- src/pessoa/entities/pessoa.entity.ts
+++ src/pessoa/entities/pessoa.entity.ts
@@ -1,1 +1,22 @@
-export class Pessoa {}
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+export class Pessoa {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column({ length: 255 })
+  passwordHash: string;
+
+  @Column({ length: 255 })
+  nome: string;
+
+  @CreateDateColumn()
+  createdAt?: Date;
+
+  @UpdateDateColumn()
+  updatedAt?: Date;
+}
