886fef64a4093655aeae16420c10b4d053ccb701
Carlos Eduardo <cadumouralemes@gmail.com>
2025-06-23 23:39:56 -0300

    feat: Adding errorhandlerinterceptor

---
+++ src/common/interceptors/error-handling.interceptor.ts
@@ -0,0 +1,21 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { resolve } from "path";
+import { catchError, Observable, tap, throwError } from "rxjs";
+
+export class ErrorHandlingInterceptor implements NestInterceptor {
+  async intercept(context: ExecutionContext, next: CallHandler<any>) {
+    const now = Date.now()
+    console.log('executado')
+
+    await new Promise(resolve => setTimeout(resolve, 10000))
+
+    return next.handle().pipe(
+      tap(() => {
+        catchError(error => {
+          console.log('deu erro')
+          return throwError(() => error)
+        })
+      })
+    )
+  }
+}
