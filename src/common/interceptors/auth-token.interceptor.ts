c4a2e4a6d36b072cab85a78d6dfe354d3ccabe90
Carlos Eduardo <cadumouralemes@gmail.com>
2025-06-25 17:15:40 -0300

    feat: Adding auth token

---
+++ src/common/interceptors/auth-token.interceptor.ts
@@ -0,0 +1,15 @@
+import { CallHandler, ExecutionContext, NestInterceptor, UnauthorizedException } from "@nestjs/common";
+import { isUndefined } from "util";
+
+export class AuthTokenInterceptor implements NestInterceptor {
+  async intercept(context: ExecutionContext, next: CallHandler<any>) {
+    const req = context.switchToHttp().getRequest()
+    const token = req.headers.authorization?.split(' ')[1]
+
+    if (typeof token === "undefined" || token !== '12345') {
+      throw new UnauthorizedException('Token invalido')
+    }
+
+    return next.handle()
+  }
+}
