920a4bc810c26389adae77572a40b374da99247c
Carlos Eduardo <cadumouralemes@gmail.com>
2025-06-26 23:15:16 -0300

    feat: Adding Othermiddleware

---
+++ src/common/middlewares/other.middleware.ts
@@ -0,0 +1,17 @@
+import { NestMiddleware } from "@nestjs/common";
+import { NextFunction, Request, Response } from "express";
+
+export class OtherMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    const auth = req.headers?.authorization
+
+    if (auth) {
+      req['user'] = {
+        nome: 'Luiz',
+        sobrenome: 'Otávio'
+      }
+    }
+
+    next()
+  }
+}
