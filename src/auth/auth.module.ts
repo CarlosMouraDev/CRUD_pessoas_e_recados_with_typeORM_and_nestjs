f32d87f2e2549a67c077d6cb88ab720553f7df57
Carlos Eduardo <cadumouralemes@gmail.com>
2025-07-08 23:19:01 -0300

    config: auth route config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,12 +1,17 @@
-import { Global, Module } from "@nestjs/common";
+import { forwardRef, Global, Module } from "@nestjs/common";
 import { HashingService } from "./hashing/hashing.service";
 import { BcryptService } from "./hashing/bcrypt.service";
 import { AuthController } from "./auth.controller";
 import { AuthService } from "./auth.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Pessoa } from "src/pessoa/entities/pessoa.entity";
 
 @Global()
 @Module({
-  controllers: [AuthController],
+  imports: [TypeOrmModule.forFeature([Pessoa])],
+  controllers: [
+    AuthController
+  ],
   providers: [
     {
       provide: HashingService,
